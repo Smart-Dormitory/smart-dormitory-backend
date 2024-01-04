d06c1fd5cb9454d4efad68d0bc3578960d2e7998
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2024-01-04 21:13:09 +0900

    feat: 공지사항 scraping 기능 추가 - resolved #27

--- nestjs/src/notice/notice.controller.ts
+++ nestjs/src/notice/notice.controller.ts
@@ -1,16 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { NoticeService } from './notice.service';
 
 @Controller('notice')
 @ApiTags('공지사항')
 export class NoticeController {
+  constructor(private readonly noticeService: NoticeService) {}
+
   @Get()
   @ApiOperation({
     summary: '공지사항',
     description: '공지사항 GET',
   })
   @ApiCreatedResponse({ description: '', type: '' })
-  noticeGet() {
-    return 'noticeGet';
+  async getAllNotice() {
+    try {
+      const notices = await this.noticeService.getAllNotice();
+      return { data: notices };
+    } catch (error) {
+      return { error: 'Failed' };
+    }
   }
 }
--- nestjs/src/notice/notice.service.ts
+++ nestjs/src/notice/notice.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { Notice } from './notice.schema';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
-export class NoticeService {}
+export class NoticeService {
+  constructor(
+    @InjectModel('Notice') private readonly noticeModel: Model<Notice>,
+  ) {}
+
+  async clearAndCreateNotice(
+    notice: Array<{
+      title: string;
+      href: string;
+    }>,
+  ): Promise<void> {
+    try {
+      // 데이터 삭제 (모든 데이터 삭제)
+      const deleteResult = await this.noticeModel.deleteMany({});
+      console.log('Delete Result:', deleteResult);
+
+      // 데이터 생성
+      const insertResult = await this.noticeModel.insertMany(notice);
+      console.log('Insert Result:', insertResult);
+    } catch (error) {
+      console.error('Error in clearAndCreateNotice:', error);
+    }
+  }
+
+  async getAllNotice(): Promise<Notice[]> {
+    return this.noticeModel.find({}, { _id: 0, __v: 0 }).exec();
+  }
+}
